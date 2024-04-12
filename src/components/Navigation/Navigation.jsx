935daf746fc4497c5a3df4d35357316a5c0d21bd
divya14190 <miniyanminiyan89@gmail.com>
2024-04-12 22:07:23 +0530

    first commit

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,20 @@
+import {Link} from "react-router-dom"
+import "./Navigation.css"
+const Navigation = ()=>{
+  return (
+    <nav>
+        <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/find-events">Find Events</Link>
+            </li>
+            <li>
+              <Link to="/">Login</Link>
+            </li>
+        </ul>
+    </nav>
+  )
+}
+export default Navigation;
